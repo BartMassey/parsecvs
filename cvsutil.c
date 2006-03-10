c5148b7cbb5e6a3f33c31379d9c1418f1063b84a
Keith Packard <keithp@neko.keithp.com>
2006-03-09 16:12:12 -0800

    Add code to compute tree from cvs file

---
+++ cvsutil.c
@@ -0,0 +1,165 @@
+/*
+ *  Copyright © 2006 Keith Packard <keithp@keithp.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or (at
+ *  your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+
+#include "cvs.h"
+
+#define min(a,b) ((a) < (b) ? (a) : (b))
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
+int
+cvs_is_head (cvs_number *n)
+{
+    return (n->c > 2 && (n->c & 1) == 0 && n->n[n->c-2] == 0);
+}
+
+int
+cvs_same_branch (cvs_number *a, cvs_number *b)
+{
+    cvs_number	t;
+    int		i;
+    int		n;
+
+    if (a->c & 1) {
+	t = *a;
+	t.n[t.c++] = 0;
+	return cvs_same_branch (b, &t);
+    }
+    if (a->c != b->c)
+	return 0;
+    n = a->c;
+    for (i = 0; i < a->c; i++)
+	if (a->n[i] != b->n[i])
+	    return 0;
+    return 1;
+}
+
+int
+cvs_number_compare (cvs_number *a, cvs_number *b)
+{
+    int n = min (a->c, b->c);
+    int i;
+
+    for (i = 0; i < n; i++) {
+	if (a->n[i] < b->n[i])
+	    return -1;
+	if (a->n[i] > b->n[i])
+	    return 1;
+    }
+    if (a->c < b->c)
+	return -1;
+    if (a->c > b->c)
+	return 1;
+    return 0;
+}
+
+int
+cvs_number_compare_n (cvs_number *a, cvs_number *b, int l)
+{
+    int n = min (l, min (a->c, b->c));
+    int i;
+
+    for (i = 0; i < n; i++) {
+	if (a->n[i] < b->n[i])
+	    return -1;
+	if (a->n[i] > b->n[i])
+	    return 1;
+    }
+    if (l > a->c)
+	return -1;
+    if (l > b->c)
+	return 1;
+    return 0;
+}
+
+cvs_number *
+cvs_previous_rev (cvs_number *n)
+{
+    cvs_number	*p = calloc (1, sizeof (cvs_number));
+    int		i;
+    
+    for (i = 0; i < p->c - 1; i++)
+	p->n[i] = n->n[i];
+    if (n->n[i] == 1) {
+	free (p);
+	return NULL;
+    }
+    p->n[i] = n->n[i] - 1;
+    return p;
+}
+
+cvs_number *
+cvs_master_rev (cvs_number *n)
+{
+    cvs_number *p = calloc (1, sizeof (cvs_number));
+
+    *p = *n;
+    p->c -= 2;
+    return p;
+}
+
+/*
+ * Find the newest revision along a specific branch
+ */
+
+cvs_number *
+cvs_branch_head (cvs_file *f, cvs_number *branch)
+{
+    cvs_number	*n = calloc (1, sizeof (cvs_number));
+    cvs_version	*v;
+
+    *n = *branch;
+    /* Check for magic branch format */
+    if ((n->c & 1) == 0 && n->n[n->c-2] == 0) {
+	n->n[n->c-2] = n->n[n->c-1];
+	n->c--;
+    }
+    for (v = f->versions; v; v = v->next) {
+	if (cvs_same_branch (n, v->number) &&
+	    cvs_number_compare (n, v->number) > 0)
+	    *n = *v;
+    }
+    return n;
+}
+
+cvs_number *
+cvs_branch_parent (cvs_file *f, cvs_number *branch)
+{
+    cvs_number	*n = calloc (1, sizeof (cvs_number));
+    cvs_version	*v;
+
+    *n = *branch;
+    n->n[n->c-1] = 0;
+    for (v = f->versions; v; v = v->next) {
+	if (cvs_same_branch (n, v->number) &&
+	    cvs_number_compare (branch, v->number) < 0 &&
+	    cvs_number_compare (n, v->number) >= 0)
+	    *n = *v;
+    }
+    return n;
+}
+
+cvs_patch *
+cvs_find_patch (cvs_file *f, cvs_number *n)
+{
+    cvs_patch	*p;
+
+    for (p = f->patches; p; p = p->next)
+	if (cvs_number_compare (p->number, n) == 0)
+	    return p;
+    return NULL;
+}
---
+++ revlist.c
@@ -0,0 +1,174 @@
+/*
+ *  Copyright © 2006 Keith Packard <keithp@keithp.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or (at
+ *  your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+
+#include "cvs.h"
+
+rev_file *
+rev_file_rev (cvs_file *cvs, cvs_number *n)
+{
+    rev_file	*f = calloc (1, sizeof (rev_file));
+    cvs_patch	*p = cvs_find_patch (cvs, n);
+
+    f->next = NULL;
+    f->name = cvs->name;
+    f->number = n;
+    if (p)
+	f->log = p->log;
+}
+
+rev_tag *
+rev_make_tag (char *name)
+{
+    rev_tag *t = calloc (1, sizeof (rev_tag));
+    t->name = name;
+    return t;
+}
+
+rev_tag *
+rev_tag_rev (cvs_file *cvs, cvs_number *branch)
+{
+    cvs_symbol	*s;
+    rev_tag_t	*tags = NULL;
+
+    for (s = cvs->symbols; s; s = s->next) {
+	if (cvs_number_compare (s->number, branch) == 0) {
+	    rev_tag_t	*t = rev_make_tag (s->name);
+	    t->next = tags;
+	    tags = t;
+	}
+    }
+    return tags;
+}
+
+rev_head *
+rev_head_cvs (cvs_file *cvs, cvs_number *branch)
+{
+    cvs_number	n;
+    rev_head	*h = calloc (1, sizeof (rev_head));
+    cvs_symbol	*s;
+    int		i;
+    cvs_version	*v;
+    rev_ent	*e;
+
+    n = *branch;
+    n.n[n.c-1] = 0;
+    while ((v = cvs_find_version (cvs, &n))) {
+	e = calloc (1, sizeof (rev_ent));
+	e->files = rev_file_rev (cvs, v->number);
+	e->tags = rev_tag_rev (cvs, v->number);
+	n = *v->number;
+    }
+    /*
+     * Locate a symbolic name for this head
+     */
+    for (s = cvs->symbols; s; s = s->next) {
+	if (cvs_is_head (s->number) && s->number->c == n.c) {
+	    for (i = 0; i < n.c - 1; i++) {
+		/* deal with wacky branch tag .0. revision format */
+		if (i == n.c - 2) {
+		    if (s->number->n[n.c - 2] != n.n[n.c - 1])
+			break;
+		} else {
+		    if (s->number->n[i] != n.n[i])
+			break;
+		}
+	    }
+	    if (i == n.c - 1) {
+		rev_tag	*t = rev_make_tag (s->name);
+		t->next = h->tags;
+		h->tags = t;
+	    }
+	}
+    }
+    return h;
+}
+
+rev_ent *
+rev_find_ent (rev_list *rl, cvs_number *number)
+{
+    rev_head	*h;
+    rev_ent	*e;
+
+    for (h = rl->heads; h; h = h->next)
+	for (e = h->ent; e && e->next; e = e->next)
+	    if (cvs_number_compare (e->number, number) == 0)
+		return e;
+    return NULL;
+}
+
+rev_list *
+rev_list_cvs (cvs_file *cvs)
+{
+    rev_list	*rl = calloc (1, sizeof (rev_list));
+    cvs_version	*cv;
+    cvs_symbol	*cs;
+    cvs_branch	*cb;
+    cvs_number	*branch_head;
+    cvs_number	*one_one = lex_number ("1.1");
+    cvs_number	*one_one_one_one = lex_number ("1.1.1.1");
+    rev_head	*trunk = rev_head_cvs (cvs, one_one);
+    rev_head	*vendor = rev_head_cvs (cvs, one_one_one_one);
+    rev_head	*h;
+    rev_ent	*e;
+    
+    /*
+     * Generate known branches
+     */
+    if (trunk) {
+	trunk->next = rl->heads;
+	rl->heads = trunk;
+    }
+    if (vendor) {
+	vendor->next = rl->heads;
+	rl->heads = vendor;
+    }
+    /*
+     * Search for other branches
+     */
+    for (cv = cvs->versions; cv; cv = cv->next) {
+	for (cb = cv->branches; cb; cb = cb->next) {
+	    branch_head = rev_head_cvs (cvs, cb->number);
+	    head = rev_head_cvs (cvs, branch_head);
+	    head->next = rl->heads;
+	    rl->heads = head;
+	}
+    }
+    /*
+     * Glue branches together
+     */
+    for (h = rl->heads; h; h = h->next) {
+	for (e = h->ent; e && e->next; e = e->next)
+	    ;
+	if (e) {
+	    for (cv = cvs->versions; cv; cv = cv->next) {
+		for (cb = cv->branches; cb; cb = cb->next) {
+		    if (cvs_number_compare (cb->number, e->number)) {
+			e->parent = rev_find_ent (rl, cv->number);
+		    }
+		}
+	    }
+	}
+    }
+    return rl;
+}
+
+rev_list *
+rev_list_merge (rev_list *a, rev_list *b)
+{
+    return NULL;
+}
